1524ffe38acfa74ce0d764aeafa61c061b9f172a
Jonesdy99 <jonesdylan1165@gmail.com>
2022-06-05 12:35:15 -0400

    implement fixes on background scroll effect

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # portfolio
+
+//added a page scroll from a Youtube video on my landing page. https://www.youtube.com/watch?v=WxWtAmZMSPE&list=LL&index=1
+
--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
+//
 var headerBg = document.getElementById('background')
 window.addEventListener('scroll',function(){
-  headerBg.style.opacity = 1 - +this.window.pageYOffset/1000+''
+  headerBg.style.opacity = 1 - +this.window.pageYOffset/1300+''
+  headerBg.style.top = +this.window.pageYOffset+'px'
+  headerBg.style.backgroundPositionY = - +this.window.pageYOffset/2+'px'
 })
+//
+
