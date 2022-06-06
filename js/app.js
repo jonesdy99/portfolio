86f3b715677eb1ac5fdf35a5908590a32cd5a019
Jonesdy99 <jonesdylan1165@gmail.com>
2022-06-06 13:55:37 -0400

    implement some mobile fixes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 //
-var headerBg = document.getElementById('background')
-window.addEventListener('scroll',function(){
-  headerBg.style.opacity = 1 - +this.window.pageYOffset/1300+''
-  headerBg.style.top = +this.window.pageYOffset+'px'
-  headerBg.style.backgroundPositionY = - +this.window.pageYOffset/2+'px'
-})
+// var headerBg = document.getElementById('background')
+// window.addEventListener('scroll',function(){
+//   headerBg.style.opacity = 1 - +this.window.pageYOffset/1300+''
+//   headerBg.style.top = +this.window.pageYOffset+'px'
+//   headerBg.style.backgroundPositionY = - +this.window.pageYOffset/2+'px'
+// })
 //
 const navSlide=() => {
   const burger = document.querySelector('.burger')
