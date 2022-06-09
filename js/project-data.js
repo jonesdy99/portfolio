06ceff6d70d6eeb594d3d8d0d04ff98ea12c338d
Jonesdy99 <jonesdylan1165@gmail.com>
2022-06-08 22:11:51 -0400

    add some button styling to project buttons

--- js/project-data.js
+++ js/project-data.js
@@ -4,7 +4,7 @@ let projectData = [
     deployment: 'https://dylanj99tictactoe.netlify.app/',
     image: '/assets/images/TicTac.jpg',
     github: 'https://github.com/jonesdy99/ttt-weekend',
-    description: 'A Tic-Tac-Toe browser game that utilizes various basic JavaScript and CSS concepts'
+    description: 'A Tic-Tac-Toe browser game that utilizes various basic JavaScript and CSS concepts.'
   },
 ]
 
