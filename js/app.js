e60974d00316deadec2c009dc520b1e672db093a
Jonesdy99 <jonesdylan1165@gmail.com>
2022-06-09 12:05:38 -0400

    implementing bug fixes

--- js/app.js
+++ js/app.js
@@ -34,7 +34,7 @@ import { projectData } from "./project-data.js"
           <h5 class="card-title">${project.title}</h5>
           <p class="card-text">${project.description}</p>
           <div>
-            <a href="${project.github}" class="btn btn-primary">GitHub</a>
+            <a href="${project.github}" class="btn btn-primary">Code</a>
             <a href="${project.deployment}" class="btn btn-secondary">Deployment</a>
           </div>
         </div>
