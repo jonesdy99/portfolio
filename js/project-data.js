a7771edeedcdf5dd72a18aa5177296c0f82331d7
Jonesdy99 <jonesdylan1165@gmail.com>
2022-06-30 19:25:28 -0400

    implement 2nd project

--- js/project-data.js
+++ js/project-data.js
@@ -6,6 +6,13 @@ let projectData = [
     github: 'https://github.com/jonesdy99/ttt-weekend',
     description: 'A Tic-Tac-Toe browser game that utilizes various basic JavaScript and CSS concepts.'
   },
+  {
+    title: 'PC-Part-Picker',
+    deployment: 'https://pc-part-picker.herokuapp.com/',
+    image: '/assets/images/ReadMe.jpg',
+    github: 'https://github.com/jonesdy99/PC-Part-Picker',
+    description: 'An app that allows users to create their own Pc through a variety of different options.'
+  },
 ]
 
 export{
